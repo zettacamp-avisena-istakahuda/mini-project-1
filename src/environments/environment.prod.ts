8e9ece577949f591b1272bdf85a055b3e0a3968d
zettacamp-avisena-istakahuda <114057605+zettacamp-avisena-istakahuda@users.noreply.github.com>
2022-12-13 10:06:21 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
 <<<<<<< HEAD
-  apiURL: 'https://b29b-182-253-183-20.ap.ngrok.io/'
+  apiURL: 'https://api.team-5.zettacamp-upn.zetta-demo.space/graphql'
 =======
-  apiURL: 'https://b013-182-253-183-7.ap.ngrok.io/'
+  apiURL: 'https://api.team-5.zettacamp-upn.zetta-demo.space/graphql'
 >>>>>>> f6bf246a1854927ce503b2488e86669402665010
 };
 
