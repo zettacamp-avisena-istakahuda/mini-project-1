5f77abe3d2c6a0519f95e0aaeb047fb3f0eb315f
zettacamp-avisena-istakahuda <114057605+zettacamp-avisena-istakahuda@users.noreply.github.com>
2022-12-13 10:26:15 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,7 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiURL: 'http://api.team-5.zettacamp-upn.zetta-demo.space/graphql'
-=======
-  apiURL: 'https://api.team-5.zettacamp-upn.zetta-demo.space/graphql'
->>>>>>> b06cb6c6be56cb45ac4767d76c006f1b8f7b9649
+
 };
 
 // http://api.team-5.zettacamp-upn.zetta-demo.space/graphql
